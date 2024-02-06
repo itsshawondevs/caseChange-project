9234cc61a1dbe489fcb248c083428a95d1008796
itsshawondevs <shawonuser24@gmail.com>
2024-02-06 12:09:32 +0600

    copy clipboard add

--- js/script.js
+++ js/script.js
@@ -8,21 +8,35 @@ let repeat = document.querySelector("#repeat ")
 
 let reset = document.querySelector("#reset")
 
-let result = document.querySelector(".result")
+let copy = document.querySelector("#copy")
+
+let tooltip = document.querySelector(".tooltip")
+
+let input = document.querySelector("#input")
 
 
 uppercase.addEventListener("click",function(){
-    result.innerHTML = `${text.value.toUpperCase()}`
+    text.value = `${text.value.toUpperCase()}`
 })
 
 lowercase.addEventListener("click",function(){
-    result.innerHTML = `${text.value.toLowerCase()}`
+    text.value = `${text.value.toLowerCase()}`
 })
 
 repeat.addEventListener("click",function(){
-    result.innerHTML = `${text.value.repeat(100)}`
+    text.value = `${text.value.repeat(100)}`
 })
 
 reset.addEventListener("click",function(){
     location.reload()
+})
+
+copy.addEventListener("click",function(){
+    text.select();
+    text.setSelectionRange(0, 99999); 
+    navigator.clipboard.writeText(text.value);
+    tooltip.innerHTML = "copied"
+    tooltip.style.width = "fit-content"
+    tooltip.style.left = "50%"
+    tooltip.style.transform = "translateX(-50%)"
 })
